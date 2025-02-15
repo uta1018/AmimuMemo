c10b57c92c85ae5261aea61b5de7e2dcc956bd07
Utaho Akashi <akashi.uta.1212@gmail.com>
2025-02-15 23:38:49 +0900

    add:ホームページの実装 #6

--- src/data/project.ts
+++ src/data/project.ts
@@ -5,7 +5,6 @@ export interface Project {
   title: string;
   createdAt: string; // 作成日
   updatedAt: Date;
-  rows: Row[];
 }
 
 interface Row {
@@ -30,131 +29,137 @@ export const dummyProjects: Project[] = [
     title: "ハートのモチーフ",
     createdAt: "2025-02-01",
     updatedAt: new Date("2025-02-01"),
-    rows: [
-      // 1段目
-      {
-        stitches: [
-          {
-            stitchType: "chain",
-            isCompleted: true,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "chain",
-            isCompleted: true,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "chain",
-            isCompleted: true,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "double",
-            isCompleted: true,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "double",
-            isCompleted: true,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "halfDouble",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "halfDouble",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "double",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 1
-          },
-          {
-            stitchType: "double",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 1
-          },
-        ]
-      },
-      // 2段目
-      {
-        stitches: [
-          {
-            stitchType: "single",
-            isCompleted: true,
-            isMarked: false,
-            comment: "",
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-          {
-            stitchType: "single",
-            isCompleted: false,
-            isMarked: false,
-            rowId: 2
-          },
-        ]
-      },
-      {
-        stitches: [
-          {
-            stitchType: "slip",
-            isCompleted: true,
-            isMarked: false,
-            comment: "輪に引き抜き編み",
-            rowId: 2
-          },
-        ]
-      },
-    ]
-  },
+    // rows: [
+    //   // 1段目
+    //   {
+    //     stitches: [
+    //       {
+    //         stitchType: "chain",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "chain",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "chain",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "double",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "double",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "halfDouble",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "halfDouble",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "double",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //       {
+    //         stitchType: "double",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 1
+    //       },
+    //     ]
+    //   },
+    //   // 2段目
+    //   {
+    //     stitches: [
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         comment: "",
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //       {
+    //         stitchType: "single",
+    //         isCompleted: false,
+    //         isMarked: false,
+    //         rowId: 2
+    //       },
+    //     ]
+    //   },
+    //   {
+    //     stitches: [
+    //       {
+    //         stitchType: "slip",
+    //         isCompleted: true,
+    //         isMarked: false,
+    //         comment: "輪に引き抜き編み",
+    //         rowId: 2
+    //       },
+    //     ]
+    //   },
+    // ]
+  },{
+    id: "2",
+    author: "uuu",
+    title: "ニット帽",
+    createdAt: "2025-02-01",
+    updatedAt: new Date("2025-02-01"),
+  }
 ];
---
+++ src/features/home/components/ProjectCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Project } from "../../../data/project";
+import ProgressBar from "../../../components/ProgressBar";
+import { PiYarn } from "react-icons/pi";
+import { GrFormNext } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
+
+interface ProjectCardProps {
+  project: Project;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => {
+        navigate(`/project/${project.id}`);
+      }}
+    >
+      <PiYarn />
+      <div>
+        {project.title}
+        <GrFormNext />
+      </div>
+      <ProgressBar progress={60} parents="home-page" />
+    </div>
+  );
+};
+
+export default ProjectCard;
