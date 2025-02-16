d127fdc481a951317ca7f6ae8a213da514ebcd3c
Utaho Akashi <akashi.uta.1212@gmail.com>
2025-02-16 11:52:39 +0900

    add:メモポップアップのCSS実装 #21

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
+import React from "react";
+import styles from "./Overlay.module.scss";
 
 interface OverlayProps {
   onClick?: () => void;
 }
 
 const Overlay: React.FC<OverlayProps> = ({ onClick }) => {
-  return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        zIndex: 4,
-      }}
-      onClick={onClick}
-    >
-    </div>
-  );
+  return <div className={styles.overlay} onClick={onClick}></div>;
 };
 
 export default Overlay;
